249bcae866e3cf5f33ac2f7226b4ca5a6ea293de
ianwoods1993 <ianwoods1993@gmail.com>
2016-11-22 07:37:11 +0000

    auto generated methods to call each letter

--- codegen.py
+++ codegen.py
@@ -1,9 +1,11 @@
-with open ("A.txt", 'r') as f:
-    lineNum = 0
-    for line in f.readlines():
-        for pos in range(0, len(line)):
-            if (line[pos] == "X"):
-                print("strip.setPixelColor(" + str(lineNum * 8 + pos) + ", Color(0, 0, 255)")
-        lineNum += 1
+for char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+    with open (str(char) + ".txt", 'r') as inFile, open(str(char) + ".py", 'w') as outFile:
+        outFile.write("def " + char + "():\n")
+        lineNum = 0
+        for line in inFile.readlines():
+            for pos in range(0, len(line)):
+                if (line[pos] == "X"):
+                    outFile.write("  strip.setPixelColor(" + str(lineNum * 8 + pos) + ", Color(0, 0, 255))\n")
+            lineNum += 1
 
-print("strip.show()")
+        outFile.write("  strip.show()\n")
