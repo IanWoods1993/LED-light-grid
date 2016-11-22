c0deb91478c09821127181a361016df3aa9b28e5
ianwoods1993 <ianwoods1993@gmail.com>
2016-11-22 07:43:02 +0000

    add in offsets for placement in string

--- codegen.py
+++ codegen.py
@@ -1,11 +1,11 @@
 for char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
     with open (str(char) + ".txt", 'r') as inFile, open(str(char) + ".py", 'w') as outFile:
-        outFile.write("def " + char + "():\n")
+        outFile.write("def " + char + "(pos):\n")
         lineNum = 0
         for line in inFile.readlines():
             for pos in range(0, len(line)):
                 if (line[pos] == "X"):
-                    outFile.write("  strip.setPixelColor(" + str(lineNum * 8 + pos) + ", Color(0, 0, 255))\n")
+                    outFile.write("  strip.setPixelColor(64 * pos + " + str(lineNum * 8 + pos) + ", Color(0, 0, 255))\n")
             lineNum += 1
 
         outFile.write("  strip.show()\n")
